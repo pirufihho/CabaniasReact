f1704aefe723dbbbf908ff81fcd39f4c8026d80e
fvalle <fvalle@ticketmundo.com>
2021-09-08 16:35:29 -0300

    //change

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ import Button from "@restart/ui/esm/Button";
       }
     render() {
       return <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3 form-control-style" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
@@ -17,14 +17,14 @@ import Button from "@restart/ui/esm/Button";
         </Form.Text>
       </Form.Group>
     
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3 form-control-style" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      <Form.Group className="mb-3 form-control-style" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button className="form-control-style" variant="primary" type="submit">
         Submit
       </Button>
     </Form>;
