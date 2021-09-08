06c1e6270060ae2eb2d701335c5cc8986d0df727
fvalle <fvalle@ticketmundo.com>
2021-09-08 17:05:22 -0300

    //changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,16 +10,16 @@ import Button from "@restart/ui/esm/Button";
     render() {
       return <Form>
       <Form.Group className="mb-3 form-control-style" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Ingresa tu email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
     
       <Form.Group className="mb-3 form-control-style" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Label>Consulta</Form.Label>
+        <Form.Control as="textarea" rows={5} placeholder="Escribe aqui tu consulta" />
       </Form.Group>
       <Form.Group className="mb-3 form-control-style" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
